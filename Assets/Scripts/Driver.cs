d31b0e249deda538b8af792464df92a83063a34c
Sebastian Lundgren <relinkz@users.noreply.github.com>
2022-01-06 18:19:47 +0100

    Adding basic movement

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField] float handlingSpeed = 100.0f;
+	[SerializeField] float baseSpeed = 4.0f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	void Update()
+	{
+		HandleMovement();
+	}
+
+	void HandleMovement()
+	{
+		var rotation = Input.GetAxis("Horizontal") * handlingSpeed * Time.deltaTime;
+		var movement = Input.GetAxis("Vertical") * baseSpeed * Time.deltaTime;
+
+		transform.Rotate(0, 0, -rotation);
+		transform.Translate(0, movement, 0);
+	}
 }
