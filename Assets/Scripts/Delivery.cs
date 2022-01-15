49b9423beaca4a7b8c160de31609eb6dae415e16
Sebastian Lundgren <relinkz@users.noreply.github.com>
2022-01-15 17:04:04 +0100

    Adding boost and pickup visuals

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -3,6 +3,15 @@ using UnityEngine;
 public class Delivery : MonoBehaviour
 {
 	bool packagePickedUp = false;
+	[SerializeField] float delayBeforeDestoryOnPickup;
+	[SerializeField] Color32 pickupColor = new Color32(0,1,0,1);
+	[SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
+	SpriteRenderer spriteRenderer;
+
+	void Start()
+	{
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
 	void OnCollisionEnter2D(Collision2D collision)
 	{
 	}
@@ -16,11 +25,16 @@ public class Delivery : MonoBehaviour
 		{
 			Debug.Log("Package picked up");
 			packagePickedUp = true;
+			Destroy(other.gameObject, delayBeforeDestoryOnPickup);
+
+			spriteRenderer.color = pickupColor;
 		}
 		else if (other.tag == "Costumer" && packagePickedUp)
 		{
 			Debug.Log("Delivered package to customer");
 			packagePickedUp = false;
+
+			spriteRenderer.color = noPackageColor;
 		}
 	}
 }
--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -4,6 +4,13 @@ public class Driver : MonoBehaviour
 {
 	[SerializeField] float handlingSpeed = 100.0f;
 	[SerializeField] float baseSpeed = 4.0f;
+	[SerializeField] float slowSpeed = 4.0f;
+	[SerializeField] float boostSpeed = 4.0f;
+
+	float collitionTimer = 0;
+	float triggerTimer = 0;
+	const float TRIGGER_LENGTH = 3.0f;
+
 
 	void Update()
 	{
@@ -12,10 +19,39 @@ public class Driver : MonoBehaviour
 
 	void HandleMovement()
 	{
+		var progressiveSlowSpeed = slowSpeed;
+		var progressiveBoostSpeed = boostSpeed;
+
+		if (collitionTimer > 0.0f)
+		{
+			collitionTimer -= Time.deltaTime;
+			progressiveSlowSpeed *= collitionTimer;
+		}
+		if (triggerTimer > 0.0f)
+		{
+			triggerTimer -= Time.deltaTime;
+			progressiveBoostSpeed *= triggerTimer;
+		}
+
+		var movementSpeed = baseSpeed + progressiveBoostSpeed - progressiveSlowSpeed;
 		var rotation = Input.GetAxis("Horizontal") * handlingSpeed * Time.deltaTime;
-		var movement = Input.GetAxis("Vertical") * baseSpeed * Time.deltaTime;
+		var movement = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
 
 		transform.Rotate(0, 0, -rotation);
 		transform.Translate(0, movement, 0);
+
+	}
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		collitionTimer = TRIGGER_LENGTH;
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.tag == "Boost")
+		{
+			triggerTimer = TRIGGER_LENGTH;
+		}
+
 	}
 }
